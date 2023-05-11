b611d583a6bc4b0b0cadd99b8ad9f3689c109e8c
RA4Z <rraz639@gmail.com>
2023-05-11 18:53:01 -0300

    Começando a criar os temas escuros e claro

---
+++ src/contexts/TemaContext.js
@@ -0,0 +1,16 @@
+import { createContext, useState } from 'react'
+import { escuro, claro } from '../estilosGlobais'
+export const TemaContext = createContext({})
+
+export function TemaProvider( {children} ) {
+const [temaAtual, setTemaAtual] = useState('escuro')
+
+  return (
+    <TemaContext.Provider value={{
+        temaAtual,
+        setTemaAtual
+    }}>
+      {children}
+    </TemaContext.Provider>
+  )
+}
